208583c13ac0d9cd7f69eb67e19ae3a9112fdaf2
Nonganba <nanganba.th1109@gmail.com>
2024-08-29 00:46:21 +0530

    homepage section

---
+++ components/Photo.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Photo = () => {
+  return (
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        {/* image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[230px] h-[230px] xl:w-[430px] xl:h-[430px] mix-blend-lighten absolute"
+          style={{ borderRadius: "50%", overflow: "hidden", border: "10px solid black"}}
+        >
+          <Image
+            src="/assets/profile-dp.png"
+            priority
+            quality={100}
+            fill
+            alt=""
+            className="object-cover"
+          />
+        </motion.div>
+
+        {/* circle */}
+        <motion.svg
+          className="w-[232px] xl:w-[432px] h-[232px] xl:h-[432px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Photo;
